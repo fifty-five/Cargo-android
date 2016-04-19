b288f2622aa2d15aae27a7d5733ae6d51c0b628b
Julien Gil <jgil@student.42.fr>
2016-04-19 14:52:25 +0200

    FEATURE-Rewriting partially the AT Internet handler

--- cargo-handlers/ATInternet/src/main/java/com/fiftyfive/cargo/handlers/ATInternetHandler.java
+++ cargo-handlers/ATInternet/src/main/java/com/fiftyfive/cargo/handlers/ATInternetHandler.java
@@ -1,25 +1,23 @@
 package com.fiftyfive.cargo.handlers;
 
 import android.app.Activity;
-import android.provider.Settings;
 import android.util.Log;
 
-import com.atinternet.tracker.SetConfigCallback;
-import com.fiftyfive.cargo.Cargo;
-import com.fiftyfive.cargo.AbstractTagHandler;
-import com.fiftyfive.cargo.models.Screen;
-import com.google.android.gms.tagmanager.Container;
 import com.atinternet.tracker.ATInternet;
+import com.atinternet.tracker.Gesture;
+import com.atinternet.tracker.SetConfigCallback;
 import com.atinternet.tracker.Tracker;
+import com.fiftyfive.cargo.AbstractTagHandler;
+import com.fiftyfive.cargo.Cargo;
+import com.fiftyfive.cargo.models.Event;
+import com.fiftyfive.cargo.models.Screen;
+import com.fiftyfive.cargo.models.User;
+import com.google.android.gms.tagmanager.Container;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import android.provider.Settings.Secure;
 
-import static com.fiftyfive.cargo.ModelsUtils.getDouble;
 import static com.fiftyfive.cargo.ModelsUtils.getInt;
-import static com.fiftyfive.cargo.ModelsUtils.getList;
 import static com.fiftyfive.cargo.ModelsUtils.getString;
 
 /**
@@ -40,8 +38,10 @@ public class ATInternetHandler extends AbstractTagHandler {
                 tagScreen(map);
                 break;
             case "AT_identify":
-                identify();
+                identify(map);
                 break;
+            case "AT_tagEvent":
+                tagEvent(map);
             default:
                 Log.i("55", "Function "+s+" is not registered");
         }
@@ -55,12 +55,31 @@ public class ATInternetHandler extends AbstractTagHandler {
         this.valid = true;
     }
 
-
     @Override
     public void register(Container container) {
         container.registerFunctionCallTagCallback("AT_tagScreen", this);
+        container.registerFunctionCallTagCallback("AT_tagEvent", this);
         container.registerFunctionCallTagCallback("AT_identify", this);
     }
+
+
+
+    // as we look for unique visitor, we use the android ID which is unique for each android device
+    private void identify(Map<String, Object> parameters){
+        if (!parameters.containsKey(User.USER_ID)) {
+            Log.w("CARGO ATInternetHandler", " in identify() missing USER_ID (android_id) parameter. USER_ID hasn't been set");
+        }
+        final String android_id = getString(parameters, User.USER_ID);
+
+        ((ATInternet) cargo.getApplication()).getDefaultTracker().setConfig("identifier", android_id, new SetConfigCallback() {
+            @Override
+            public void setConfigEnd() {
+                Log.i("atInternetHandler", "SDK is now using Android ID as visitor identifier : " + android_id);
+            }
+        });
+
+    }
+
 
     private void tagScreen(Map<String, Object> parameters){
 
@@ -77,22 +96,51 @@ public class ATInternetHandler extends AbstractTagHandler {
                 put(com.fiftyfive.cargo.models.Tracker.CUSTOM_DIM2, customDim2);
             }});
         }
-        atTracker.Screens().add(screenName).setLevel2(getInt(parameters, com.fiftyfive.cargo.models.Tracker.LEVEL2, 0)).sendView();
+        atTracker.Screens()
+                .add(screenName)
+                .setLevel2(getInt(parameters, com.fiftyfive.cargo.models.Tracker.LEVEL2, 0))
+                .sendView();
     }
 
 
-    // as we look for unique visitor, we use the android ID which is unique for each android device
-    private void identify(){
-        final String android_id = Settings.Secure.getString(cargo.getApplication().getContentResolver(), Secure.ANDROID_ID);
+    private void tagEvent(Map<String, Object> parameters){
+        Gesture gesture;
+        String eventName = getString(parameters, Event.EVENT_NAME);
+        String eventType = getString(parameters, Event.EVENT_TYPE);
 
-        ((ATInternet) cargo.getApplication()).getDefaultTracker().setConfig("identifier", android_id, new SetConfigCallback() {
-            @Override
-            public void setConfigEnd() {
-                Log.i("atInternetHandler", "SDK is now using Android ID as visitor identifier : " + android_id);
-            }
-        });
+        if (eventType == null)
+            Log.w("CARGO ATInternetHandler", "in tagEvent() no EVENT_TYPE given, event hasn't been sent");
+
+        // we don't even need to check if chapters are set
+        // because in the gesture object, each chapter is set to null by default
+        // and if getString fails, it returns null
+        gesture =  atTracker.Gestures().add(eventName, getString(parameters, "chapter1"), getString(parameters, "chapter2"), getString(parameters, "chapter3"));
+
+        gesture.setLevel2(getInt(parameters, com.fiftyfive.cargo.models.Tracker.LEVEL2, 0));
+
+        switch (eventType) {
+            case "sendTouch":
+                gesture.sendTouch();
+                break;
+            case "sendNavigation":
+                gesture.sendNavigation();
+                break;
+            case "sendDownload":
+                gesture.sendDownload();
+                break;
+            case "sendExit":
+                gesture.sendExit();
+                break;
+            case "sendSearch":
+                gesture.sendSearch();
+                break;
+            default:
+                Log.w("CARGO ATInternetHandler", "in tagEvent() wrong EVENT_TYPE given, event hasn't been sent");
+                break;
+        }
 
     }
+
 
 
     @Override
