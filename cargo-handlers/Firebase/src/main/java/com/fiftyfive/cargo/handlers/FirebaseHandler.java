ee7953e8165d960f0c2b2a2dee46af6244749d78
Julien Gil <jgil@student.42.fr>
2016-07-13 18:31:04 +0200

    implementation of firebase handler

--- cargo-handlers/Firebase/src/main/java/com/fiftyfive/cargo/handlers/FirebaseHandler.java
+++ cargo-handlers/Firebase/src/main/java/com/fiftyfive/cargo/handlers/FirebaseHandler.java
@@ -1,13 +1,18 @@
 package com.fiftyfive.cargo.handlers;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.util.Log;
 
 import com.fiftyfive.cargo.AbstractTagHandler;
+import com.fiftyfive.cargo.models.Event;
+import com.fiftyfive.cargo.models.User;
 import com.google.android.gms.tagmanager.Container;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 import java.util.Map;
+
+import static com.fiftyfive.cargo.ModelsUtils.getString;
 
 
 /**
@@ -42,6 +47,7 @@ public class FirebaseHandler extends AbstractTagHandler {
     @Override
     public void register(Container container) {
         container.registerFunctionCallTagCallback("Firebase_tagEvent", this);
+        container.registerFunctionCallTagCallback("Firebase_identify", this);
     }
 
 
@@ -52,16 +58,42 @@ public class FirebaseHandler extends AbstractTagHandler {
             case "Firebase_tagEvent":
                 tagEvent(map);
                 break;
+            case "Firebase_identify":
+                identify(map);
+                break;
             default:
                 Log.i("Cargo TuneHandler", "Function "+s+" is not registered");
         }
     }
-
-
+    
     private void tagEvent(Map<String, Object> map) {
 
+        if (map.containsKey(Event.EVENT_NAME)) {
+            String eventName = map.remove(Event.EVENT_NAME).toString();
+            Bundle params = new Bundle();
+
+            for (Map.Entry<String, Object> entry : map.entrySet()) {
+                if (entry.getValue() instanceof String)
+                    params.putString(entry.getKey(), map.remove(entry.getKey()).toString());
+                else if (entry.getValue() instanceof Long)
+                    params.putLong(entry.getKey(), (long)map.remove(entry.getKey()));
+            }
+            mFirebaseAnalytics.logEvent(eventName, params);
+        }
+        else
+            Log.w("Cargo FirebaseHandler", " in order to create an event, " +
+                    "an eventName is required. The event hasn't been created.");
     }
 
+    private void identify(Map<String, Object> map) {
+        if (map.containsKey(User.USER_ID)) {
+            mFirebaseAnalytics.setUserId(getString(map, User.USER_ID));
+            map.remove(User.USER_ID);
+        }
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            mFirebaseAnalytics.setUserProperty(entry.getKey(), getString(map, entry.getKey()));
+        }
+    }
 
     @Override
     public void onActivityStarted(Activity activity) {
@@ -71,7 +103,7 @@ public class FirebaseHandler extends AbstractTagHandler {
 
     @Override
     public void onActivityResumed(Activity activity) {
-
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.APP_OPEN, new Bundle());
     }
 
     @Override
