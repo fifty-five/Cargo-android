8ff54750d5ace402d53c2f5fa33230ec412b1a82
juliengil <julien.gil@fifty-five.com>
2016-10-21 10:57:15 +0200

    minor changes in accengage handler class

--- cargo-handlers/Accengage/src/main/java/com/fiftyfive/cargo/handlers/AccengageHandler.java
+++ cargo-handlers/Accengage/src/main/java/com/fiftyfive/cargo/handlers/AccengageHandler.java
@@ -1,12 +1,15 @@
 package com.fiftyfive.cargo.handlers;
 
 import android.app.Activity;
+import android.content.Context;
 import android.util.Log;
 
 import com.fiftyfive.cargo.AbstractTagHandler;
 import com.google.android.gms.tagmanager.Container;
 
 import java.util.Map;
+
+import static com.fiftyfive.cargo.ModelsUtils.getString;
 
 /**
  * Created by Julien Gil on 12/10/16.
@@ -15,6 +18,9 @@ import java.util.Map;
 public class AccengageHandler extends AbstractTagHandler {
 
     final String ACC_init = "ACC_init";
+
+    private String partnerId = null;
+    private String privateKey = null;
 
     @Override
     public void register(Container container) {
@@ -35,22 +41,28 @@ public class AccengageHandler extends AbstractTagHandler {
 
     @Override
     public void initialize() {
+        super.initialize();
         //todo : check permissions
         this.valid = true;
     }
 
-    public void init(Map<String, Object> map) {
-
+    private void init(Map<String, Object> map) {
+        if (!map.containsKey("privateKey") || !map.containsKey("partnerId")) {
+            Log.w("Cargo AccengageHandler", " partnerId and/or privateKey is missing for " +
+                    "the Accengage SDK initialization");
+        }
+        else {
+            privateKey = getString(map, "privateKey");
+            partnerId = getString(map, "partnerId");
+        }
     }
 
     @Override
     public void onActivityStarted(Activity activity) {
-
     }
 
     @Override
     public void onActivityResumed(Activity activity) {
-
     }
 
     @Override
@@ -62,5 +74,4 @@ public class AccengageHandler extends AbstractTagHandler {
     public void onActivityStopped(Activity activity) {
 
     }
-
 }
