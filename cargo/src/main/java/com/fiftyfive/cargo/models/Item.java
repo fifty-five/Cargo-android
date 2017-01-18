81ba9f5874c509a8498d9973b81618f0f926ab40
Julien Gil <julien.gil@fifty-five.com>
2017-01-18 14:48:11 +0100

    class Item filled

--- cargo/src/main/java/com/fiftyfive/cargo/models/Item.java
+++ cargo/src/main/java/com/fiftyfive/cargo/models/Item.java
@@ -4,5 +4,24 @@ package com.fiftyfive.cargo.models;
  * Created by Julien Gil on 18/01/2017.
  */
 
-public class Item {
+public final class Item {
+    public final static String NAME = "name";
+    public final static String ID = "id";
+    public final static String UNIT_PRICE = "unitPrice";
+    public final static String QUANTITY = "quantity";
+    public final static String REVENUE = "revenue";
+    public final static String BRAND = "brand";
+    public final static String CATEGORY = "category";
+    public final static String VARIANT = "variant";
+    public final static String POSITION = "position";
+    public final static String COUPON_CODE = "couponCode";
+    public final static String INDEX_DIM = "indexDim";
+    public final static String VALUE_DIM = "valueDim";
+    public final static String INDEX_METRIC = "indexMetric";
+    public final static String VALUE_METRIC = "valueMetric";
+    public final static String ATTR1 = "attr1";
+    public final static String ATTR2 = "attr2";
+    public final static String ATTR3 = "attr3";
+    public final static String ATTR4 = "attr4";
+    public final static String ATTR5 = "attr5";
 }
