b4875f69be5ccad9b7f2814a0322b230e6e53878
Julien Gil <julien.gil@fifty-five.com>
2017-02-01 11:36:04 +0100

    updated app to make cargo more testable

--- app/src/main/java/com/fiftyfive/cargo_android/MainActivity.java
+++ app/src/main/java/com/fiftyfive/cargo_android/MainActivity.java
@@ -1,17 +1,28 @@
 package com.fiftyfive.cargo_android;
 
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import com.fiftyfive.cargo.Cargo;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.tune.TuneEventItem;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAnalytics mFirebaseAnalytics;
+    String username;
+    String userMail;
+    EditText userText;
+    EditText mailAdressText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
         Button tagScreenButton = (Button) findViewById(R.id.tagScreenButton);
         Button setUserButton = (Button) findViewById(R.id.identifyButton);
         Button tagPurchaseButton = (Button) findViewById(R.id.tagPurchaseButton);
+        userText = (EditText)findViewById(R.id.usernameInput);
+        mailAdressText = (EditText)findViewById(R.id.mailAdressInput);
+
         tagEventButton.setOnClickListener(tagEventListener);
         tagScreenButton.setOnClickListener(tagScreenListener);
         setUserButton.setOnClickListener(setUserListener);
@@ -33,7 +47,18 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener tagEventListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            mFirebaseAnalytics.logEvent("tagEvent", null);
+
+            Bundle bundle = new Bundle();
+
+            username = userText.getText().toString();
+            userMail = mailAdressText.getText().toString();
+
+            if (username.length() > 0)
+                bundle.putString("username", username);
+            if (userMail.length() > 0)
+                bundle.putString("userEmail", userMail);
+
+            mFirebaseAnalytics.logEvent("tagEvent", bundle);
         }
     };
 
@@ -47,14 +72,48 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener setUserListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            mFirebaseAnalytics.logEvent("identify", null);
+
+            Bundle bundle = new Bundle();
+            username = userText.getText().toString();
+            userMail = mailAdressText.getText().toString();
+
+            if (username.length() > 0)
+                bundle.putString("username", username);
+            if (userMail.length() > 0)
+                bundle.putString("userEmail", userMail);
+
+            mFirebaseAnalytics.logEvent("identify", bundle);
         }
     };
 
     View.OnClickListener tagPurchaseListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            mFirebaseAnalytics.logEvent("tagPurchase", null);
+            Bundle bundle = new Bundle();
+            Double revenue = 0.0;
+
+            username = userText.getText().toString();
+            userMail = mailAdressText.getText().toString();
+
+            CheckBox xboxBox = (CheckBox)findViewById(R.id.boxXbox);
+            CheckBox playBox = (CheckBox)findViewById(R.id.boxPlaystation);
+            CheckBox nintendoBox = (CheckBox)findViewById(R.id.boxNintendo);
+
+            if (username.length() > 0)
+                bundle.putString("username", username);
+            if (userMail.length() > 0)
+                bundle.putString("userEmail", userMail);
+
+            if (xboxBox.isChecked())
+                revenue += 149.99;
+            if (playBox.isChecked())
+                revenue += 199;
+            if (nintendoBox.isChecked())
+                revenue += 255;
+
+            bundle.putString("currencyCode", "EUR");
+            bundle.putDouble("totalRevenue", revenue);
+            mFirebaseAnalytics.logEvent("tagPurchase", bundle);
         }
     };
 }
