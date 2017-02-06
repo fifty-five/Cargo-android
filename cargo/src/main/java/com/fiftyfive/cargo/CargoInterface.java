d1b152a67613e442fab82653fdf3bd6edef5a79d
Julien Gil <julien.gil@fifty-five.com>
2017-02-06 11:54:38 +0100

    the interface to implement to register to the callback

---
+++ cargo/src/main/java/com/fiftyfive/cargo/CargoInterface.java
@@ -0,0 +1,11 @@
+package com.fiftyfive.cargo;
+
+/**
+ * Created by Julien Gil on 06/02/2017.
+ * Copyright 2016 fifty-five All rights reserved.
+ *
+ * The interface to implement in order to be warned when Cargo is ready
+ */
+public interface CargoInterface {
+    void isReady();
+}
