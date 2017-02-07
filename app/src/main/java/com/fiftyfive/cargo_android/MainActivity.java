121d0d42a534678333e43d04f084e02c801c9bc1
Julien Gil <julien.gil@fifty-five.com>
2017-02-07 17:05:41 +0100

    updated mainActivity

--- app/src/main/java/com/fiftyfive/cargo_android/MainActivity.java
+++ app/src/main/java/com/fiftyfive/cargo_android/MainActivity.java
@@ -2,9 +2,12 @@ package com.fiftyfive.cargo_android;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import com.fiftyfive.cargo.CargoItem;
@@ -18,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     String userMail;
     EditText userText;
     EditText mailAdressText;
+    EditText xboxNumber;
+    EditText playNumber;
+    EditText nintendoNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,49 +80,60 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             Bundle bundle = new Bundle();
             Double revenue = 0.0;
-            username = userText.getText().toString();
-            userMail = mailAdressText.getText().toString();
-            CheckBox xboxBox = (CheckBox)findViewById(R.id.boxXbox);
-            CheckBox playBox = (CheckBox)findViewById(R.id.boxPlaystation);
-            CheckBox nintendoBox = (CheckBox)findViewById(R.id.boxNintendo);
+            int xboxQty = getQty(xboxNumber);
+            int playQty = getQty(playNumber);
+            int nintendoQty = getQty(nintendoNumber);
 
-            if (username.length() > 0)
-                bundle.putString("username", username);
-            if (userMail.length() > 0)
-                bundle.putString("userEmail", userMail);
-
-            if (xboxBox.isChecked()) {
-                revenue += 149.99;
-                CargoItem.attachItemToEvent(new CargoItem("xboxBox").setUnitPrice(149.99).setQuantity(1).setAttribute1("attr1TESTforTune"));
+            if (xboxQty > 0) {
+                revenue += (149.99 * xboxQty);
+                CargoItem.attachItemToEvent(
+                        new CargoItem("xBox One").setUnitPrice(149.99).setQuantity(xboxQty).setAttribute1("Microsoft")
+                );
             }
-            if (playBox.isChecked()) {
-                revenue += 199;
-                CargoItem.attachItemToEvent(new CargoItem("playBox").setUnitPrice(199).setQuantity(1).setAttribute1("attr1TESTforTune"));
+            if (playQty > 0) {
+                revenue += (199 * playQty);
+                CargoItem.attachItemToEvent(
+                        new CargoItem("PlayStation 4").setUnitPrice(199).setQuantity(playQty).setAttribute1("Sony")
+                );
             }
-            if (nintendoBox.isChecked()) {
-                revenue += 255;
-                CargoItem.attachItemToEvent(new CargoItem("nintendoBox").setUnitPrice(255).setQuantity(1).setAttribute1("attr1TESTforTune"));
+            if (nintendoQty > 0) {
+                revenue += (255 * nintendoQty);
+                CargoItem.attachItemToEvent(
+                        new CargoItem("Nintendo Switch").setUnitPrice(255).setQuantity(nintendoQty).setAttribute1("Nintendo")
+                );
             }
 
             bundle.putString("currencyCode", "EUR");
             bundle.putDouble("totalRevenue", revenue);
-            bundle.putBoolean("eventItems", true);
+            if (CargoItem.itemsList != null && CargoItem.itemsList.size() != 0)
+                bundle.putBoolean("eventItems", true);
             mFirebaseAnalytics.logEvent("tagPurchase", bundle);
         }
     };
 
+    int getQty(EditText eTxt) {
+        String str = eTxt.getText().toString();
+        if (str.length() == 0)
+            return 0;
+        return Integer.parseInt(str);
+    }
+
     void setupUI() {
         Button tagEventButton = (Button) findViewById(R.id.tagEventButton);
+        tagEventButton.setOnClickListener(tagEventListener);
         Button tagScreenButton = (Button) findViewById(R.id.tagScreenButton);
+        tagScreenButton.setOnClickListener(tagScreenListener);
         Button setUserButton = (Button) findViewById(R.id.identifyButton);
+        setUserButton.setOnClickListener(setUserListener);
         Button tagPurchaseButton = (Button) findViewById(R.id.tagPurchaseButton);
+        tagPurchaseButton.setOnClickListener(tagPurchaseListener);
 
         userText = (EditText)findViewById(R.id.usernameInput);
         mailAdressText = (EditText)findViewById(R.id.mailAdressInput);
 
-        tagEventButton.setOnClickListener(tagEventListener);
-        tagScreenButton.setOnClickListener(tagScreenListener);
-        setUserButton.setOnClickListener(setUserListener);
-        tagPurchaseButton.setOnClickListener(tagPurchaseListener);
+        xboxNumber = (EditText)findViewById(R.id.editTextXbox);
+        playNumber = (EditText)findViewById(R.id.editTextPlay);
+        nintendoNumber = (EditText)findViewById(R.id.editTextNintendo);
     }
+
 }
